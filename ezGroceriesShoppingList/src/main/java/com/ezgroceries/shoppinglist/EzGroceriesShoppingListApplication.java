0d1d0bd7e9800080a31c14a25203badbdf0e273b
u90324 <an.dewit@kbc.be>
2022-10-27 10:55:38 +0200

    lab1

---
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzGroceriesShoppingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzGroceriesShoppingListApplication.class, args);
+    }
+
+}
