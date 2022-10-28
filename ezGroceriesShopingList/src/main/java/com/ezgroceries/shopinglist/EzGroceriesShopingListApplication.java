67fa2be3ac526a1d0d83a7267ed22cd4e0a4db7e
jf13300 <svetoslav.valev@kbc.com>
2022-10-28 10:57:46 +0300

    lab-01-spring-boot | init application

---
+++ ezGroceriesShopingList/src/main/java/com/ezgroceries/shopinglist/EzGroceriesShopingListApplication.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shopinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzGroceriesShopingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzGroceriesShopingListApplication.class, args);
+    }
+
+}
---
+++ ezGroceriesShopingList/src/test/java/com/ezgroceries/shopinglist/EzGroceriesShopingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shopinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzGroceriesShopingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
