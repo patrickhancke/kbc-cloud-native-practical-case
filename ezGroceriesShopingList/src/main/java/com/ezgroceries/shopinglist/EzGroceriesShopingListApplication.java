c529ce0496ddcf22bb2f92ba8bb2ea83717a9476
jf13300 <svetoslav.valev@kbc.com>
2022-11-16 12:10:33 +0200

    lab-06-spring-boot

--- ezGroceriesShopingList/src/main/java/com/ezgroceries/shopinglist/EzGroceriesShopingListApplication.java
+++ ezGroceriesShopingList/src/main/java/com/ezgroceries/shopinglist/EzGroceriesShopingListApplication.java
@@ -1,8 +1,13 @@
 package com.ezgroceries.shopinglist;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,6 +15,16 @@ public class EzGroceriesShopingListApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EzGroceriesShopingListApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort){
+        Connector connector=new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
 
 }
