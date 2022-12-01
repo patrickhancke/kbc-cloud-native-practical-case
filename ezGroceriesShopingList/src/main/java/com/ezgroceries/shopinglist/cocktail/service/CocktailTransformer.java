38b6f8229a9cafd5612161bc9252d04bc5f4246b
jf13300 <svetoslav.valev@kbc.com>
2022-12-01 15:40:35 +0200

    fix review remarks

--- ezGroceriesShopingList/src/main/java/com/ezgroceries/shopinglist/cocktail/service/CocktailTransformer.java
+++ ezGroceriesShopingList/src/main/java/com/ezgroceries/shopinglist/cocktail/service/CocktailTransformer.java
@@ -3,13 +3,8 @@ package com.ezgroceries.shopinglist.cocktail.service;
 import com.ezgroceries.shopinglist.cocktail.Cocktail;
 import com.ezgroceries.shopinglist.cocktail.persistence.CocktailEntity;
 import java.net.URI;
-import java.net.URISyntaxException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CocktailTransformer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CocktailTransformer.class);
-
     public static Cocktail transform(CocktailEntity entity) {
         Cocktail result = new Cocktail();
         result.setCocktailId(entity.getId());
@@ -18,11 +13,7 @@ public class CocktailTransformer {
         result.setInstructions(entity.getInstructions());
 
         if (entity.getImage() != null) {
-            try {
-                result.setImage(new URI(entity.getImage()));
-            } catch (URISyntaxException e) {
-                LOGGER.error(e.getMessage());
-            }
+            result.setImage(URI.create(entity.getImage()));
         }
 
         if (entity.getIngredients() != null) {
