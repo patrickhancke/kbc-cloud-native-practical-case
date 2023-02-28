c2a0fd1bb4903b9a34b2cb53e4a9e8b4b9f6b152
Tami Apteker <tami.apteker@kbc.be>
2023-02-28 09:51:35 +0100

    Lab 01 - Spring Boot

---
+++ ezGroceriesShoppingList/src/test/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzGroceriesShoppingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
